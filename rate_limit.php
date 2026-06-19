f496a9b1015ce074c39a9e831202cc824e8e9732
tashfinsami <tashfinsami@gmail.com>
2026-06-19 20:34:34 +0600

    feat: Change rate limiting algorithm from fixed window to sliding window log

--- rate_limit.php
+++ rate_limit.php
@@ -1,27 +1,42 @@
 <?php
 
+/* =========================
+   RATE LIMITER 
+   (SLIDING WINDOW LOG)
+========================= */
 function rateLimit($redis, $key, $limit = 60, $window = 60)
 {
-    $redisKey = "rate:" . md5($key);
+    $redisKey = "rl:" . md5($key);
 
-    // increment request count
-    $count = $redis->incr($redisKey); //atomic increment
+    $now = microtime(true); //count in millisecs
 
-    // set expiry only on first request
-    if ($count == 1) {
-        $redis->expire($redisKey, $window); //key deleted automatically after time expires
-    }
+    // remove old requests outside the window (on sorted list)
+    $redis->zRemRangeByScore($redisKey, 0, $now - $window); // (key, min_score, max_score)
 
-    // check limit
-    if ($count > $limit) {
+    // count requests in current window
+    $count = $redis->zCard($redisKey);
+
+    // if limit exceeded
+    if ($count >= $limit) {
+        // get oldest request timestamp for retry info
+        $oldest = $redis->zRange($redisKey, 0, 0, true); // (key, index, total_vals,  return score or not)
+
+        $oldestTime = $oldest ? array_values($oldest)[0] : $now; // assign oldest timestamp or current timestamp(first req)
+
         return [
             "allowed" => false,
-            "retry_after" => $redis->ttl($redisKey) //time to live
+            "retry_after" => ceil(($oldestTime + $window) - $now)
         ];
     }
 
+    // add current request
+    $redis->zAdd($redisKey, $now, uniqid("", true)); // (key, timestamp, unique req id)
+
+    // cleanup (to save memory)
+    $redis->expire($redisKey, $window); // delete key as not needed after (last timestamp + window) secs
+
     return [
         "allowed" => true,
-        "count" => $count
+        "count" => $count + 1
     ];
 }
